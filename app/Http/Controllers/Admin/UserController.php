ffa770ffc066e802d0c6d56ffcec90e3977a0ed2
9jorgearias <jmarias073@misena.edu.co>
2022-11-10 16:07:38 -0500

    actualizacion seeders

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -10,16 +10,16 @@ use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
-   public function __construct()
-   {
-        $this->middleware('can:admin.users.index')->only('index');
-        $this->middleware('can:admin.users.edit')->only('edit', 'update');
-        $this->middleware('can:admin.users.destroy')->only('destroy');
-   }
+//    public function __construct()
+//    {
+//         $this->middleware('can:admin.users.index')->only('index');
+//         $this->middleware('can:admin.users.edit')->only('edit', 'update');
+//         $this->middleware('can:admin.users.destroy')->only('destroy');
+//    }
 
     public function index()
     {
-        $users = User::all();
+        //$users = User::all();
         return view('admin.users.index');
     }
 
