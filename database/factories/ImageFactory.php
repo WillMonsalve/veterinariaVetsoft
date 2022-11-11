75eaeec382e783a4e350f64d0cc9425dd1c26229
9jorgearias <jmarias073@misena.edu.co>
2022-11-11 18:03:21 -0500

    actual3

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -5,22 +5,6 @@ namespace Database\Factories;
 use App\Models\Image;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
- */
-class ImageFactory extends Factory
-{
-    protected $model = Image::class;
-<?php
-
-namespace Database\Factories;
-
-use App\Models\Image;
-use Illuminate\Database\Eloquent\Factories\Factory;
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
- */
 class ImageFactory extends Factory
 {
     protected $model = Image::class;
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -8,29 +8,8 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
- */
-class PostFactory extends Factory
-{ 
-    protected $model = Post::class;
 
-    public function definition()
-    {
-        $name = $this->faker->unique()->sentence();
-<?php
 
-namespace Database\Factories;
-
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
- */
 class PostFactory extends Factory
 { 
     protected $model = Post::class;
