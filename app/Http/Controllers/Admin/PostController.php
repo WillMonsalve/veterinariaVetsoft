fdc33cd3bc1aac532ac092f0d7b70faa1f6ff411
willmonsalve <wmonsalve85@gmail.com>
2022-11-24 09:11:19 -0500

    post

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,48 +4,48 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Post;
 class PostController extends Controller
 {
     
     public function index()
     {
-        //
+        
     }
 
    
     public function create()
     {
-        //
+        
     }
 
     
     public function store(Request $request)
     {
-        //
+        
     }
 
     
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+        
     }
 
     
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        
     }
 
    
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        
     }
 
     
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        
     }
 }
