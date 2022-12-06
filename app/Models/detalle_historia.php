eaed93a0ed1b4456b68237785ce3d71405803f31
9jorgearias <jmarias073@misena.edu.co>
2022-12-06 12:18:29 -0500

    detalles

--- app/Models/detalle_historia.php
+++ app/Models/detalle_historia.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class historia extends Model
+class detalle_historia extends Model
 {
     use HasFactory;
 }
