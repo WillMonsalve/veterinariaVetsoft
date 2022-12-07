ac4b1fcd6f56a54ddaa047c0f18e05d3c08c997d
Fabiana1235 <fabiana.loaiza@misena.edu.co>
2022-12-06 22:16:01 -0500

    ULTIMO

--- resources/views/admin/ayudas/index.blade.php
+++ resources/views/admin/ayudas/index.blade.php
@@ -3,8 +3,10 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 <h1 style="text-align:center">AYUDAS</h1>
+<hr>
+<p class= text-align:center>Tienes una duda ingresa a las opciones para tener una mejor asesoria</p>
 @stop
 
 @section('content')
@@ -14,7 +16,7 @@
         <div class="card-body">
           <h5 class="card-title">¿Olvidaste tu contraseña?</h5>
           <p class="card-text">Si olvidaste tu contraseña puedes solicitar mediante la opción, olvide mi contraseña que se encuentra en el formulario de login de la página.</p>
-          <a href="#" class="btn btn-primary">Ver más</a>
+          <a href="#"class="btn btn-success"><i class="bi bi-info-circle-fill"></i></a>
         </div>
       </div>
     </div>
@@ -22,8 +24,8 @@
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">¿Como registrarme? </h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Ver más</a>
+          <p class="card-text"> </p>
+          <a href="#" class="btn btn-success"><i class="bi bi-info-circle-fill"></i></a>
         </div>
       </div>
     </div>
@@ -34,37 +36,17 @@
         <div class="card-body">
           <h5 class="card-title">¿Como solicitar una cita? </h5>
           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Ver más</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">¿Nuestros datos de contacto?</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Ver más</a>
+          <a href="#" class="btn btn-success"><i class="bi bi-info-circle-fill"></i></a>
         </div>
       </div>
     </div>
   </div>
-
-  <div class="row">
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">¿Como solicitar una cita? </h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Ver más</a>
-        </div>
-      </div>
-    </div>
     <div class="col-sm-6">
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">¿Nuestros datos de contacto?</h5>
           <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Ver más</a>
+          <a href="https://es-la.facebook.com/acatdogtor/" class="btn btn-success"><i class="bi bi-info-circle-fill"></i></a>
         </div>
       </div>
     </div>
