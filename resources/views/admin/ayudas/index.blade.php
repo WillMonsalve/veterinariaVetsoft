805abf14e227536576c84985d9f79b526a8f7562
JuanTrP <juan.fe19@hotmail.com>
2022-12-08 15:28:36 -0500

    Ayudas Ok

--- resources/views/admin/ayudas/index.blade.php
+++ resources/views/admin/ayudas/index.blade.php
@@ -1,55 +1,193 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Ayudas')
+
+@section('cs')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+@endsection
 
 @section('content_header')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
-<h1 style="text-align:center">AYUDAS</h1>
-<hr>
-<p>¿Necesitas ayuda? Ingresa a las opciones para tener una mejor asesoría.</p>
+
+    <h1 style="text-align:center">AYUDAS</h1>
+    <h5 style="text-align:center">
+        ¿Necesitas ayuda?, Ingresa a las opciones para tener una mejor asesoría.
+    </h5>
+    <br>
+    <br>
+    <br>
+    <br>
 @stop
 
 @section('content')
-<div class="row">
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">¿Olvidaste tu contraseña?</h5>
-          <p class="card-text"></p>
-          <a href="#"class="btn btn-success"><i class="bi bi-info-circle-fill"></i></a>
-        </div>
-      </div>
-    </div>
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">¿Como registrarme? </h5>
-          <p class="card-text"> </p>
-          <a href="#" class="btn btn-success"><i class="bi bi-info-circle-fill"></i></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">¿Como solicitar una cita? </h5>
-          <p class="card-text"></p>
-          <a href="#" class="btn btn-success"><i class="bi bi-info-circle-fill"></i></a>
-        </div>
-      </div>
-    </div>
-  </div>
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">¿Nuestros datos de contacto?</h5>
-          <p class="card-text"></p>
-          <a href="https://es-la.facebook.com/acatdogtor/" class="btn btn-success"><i class="bi bi-info-circle-fill"></i></a>
-        </div>
-      </div>
-    </div>
-  </div>
 
-@stop
+
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <div class="ratio ratio-4x3">
+                    <iframe width="288" height="200" src="https://www.youtube.com/embed/diIOyfWTaB0"
+                        title="Teaser video" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+                gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Olvidaste tu contraseña?</h5>
+                    <br>
+                    <br>
+                    <h6 class="card-text">Si olvidaste tu contraseña en el video te explicamos como puedes recuperarla.</h6>
+                    <a href="https://www.youtube.com/embed/diIOyfWTaB0" class="btn btn-primary">Video</a>
+                </div>
+            </div>
+        </div>
+
+
+
+            <div class="col-sm-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="ratio ratio-4x3">
+                        <iframe width="288" height="200" src="https://www.youtube.com/embed/diIOyfWTaB0"
+                            title="Teaser video" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+                gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Como registrar una cita?</h5>
+                        <br>
+                        <br>
+                        <h6 class="card-text">Si no sabes como registrar una cita en el video te explicamos como puedes
+                            hacerlo.</h6>
+                        <a href="https://www.youtube.com/embed/diIOyfWTaB0" class="btn btn-primary">Video</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-sm-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="ratio ratio-4x3">
+                        <iframe width="288" height="200" src="https://www.youtube.com/embed/diIOyfWTaB0"
+                            title="Teaser video" frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+                gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Como registrarme?</h5>
+                        <br>
+                        <br>
+                        <h6 class="card-text">Si no sabes como registrarte en el video te explicamos como puedes hacerlo.
+                        </h6>
+                        <a href="https://www.youtube.com/embed/diIOyfWTaB0" class="btn btn-primary">Video</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+              <div class="card" style="width: 18rem;">
+                  <div class="ratio ratio-4x3">
+                      <iframe width="288" height="200" src="https://www.youtube.com/embed/diIOyfWTaB0"
+                          title="Teaser video" frameborder="0"
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+              gyroscope; picture-in-picture"
+                          allowfullscreen></iframe>
+                  </div>
+                  <div class="card-body">
+                      <h5 class="card-title">Como crear en post en la landing page?</h5>
+                      <br>
+                      <br>
+                      <h6 class="card-text">Si no sabes como crear un post en la landing page en el video te explicamos como puedes hacerlo.
+                      </h6>
+                      <a href="https://www.youtube.com/embed/diIOyfWTaB0" class="btn btn-primary">Video</a>
+                  </div>
+              </div>
+          </div>
+          /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+          <div class="col-sm-3">
+            <div class="card" style="width: 18rem;">
+                <div class="ratio ratio-4x3">
+                    <iframe width="288" height="200" src="https://www.youtube.com/embed/diIOyfWTaB0"
+                        title="Teaser video" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+            gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Como crear en post en la landing page?</h5>
+                    <br>
+                    <br>
+                    <h6 class="card-text">Si no sabes como crear un post en la landing page en el video te explicamos como puedes hacerlo.
+                    </h6>
+                    <a href="https://www.youtube.com/embed/diIOyfWTaB0" class="btn btn-primary">Video</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+          <div class="card" style="width: 18rem;">
+              <div class="ratio ratio-4x3">
+                  <iframe width="288" height="200" src="https://www.youtube.com/embed/diIOyfWTaB0"
+                      title="Teaser video" frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+          gyroscope; picture-in-picture"
+                      allowfullscreen></iframe>
+              </div>
+              <div class="card-body">
+                  <h5 class="card-title">Como crear en post en la landing page?</h5>
+                  <br>
+                  <br>
+                  <h6 class="card-text">Si no sabes como crear un post en la landing page en el video te explicamos como puedes hacerlo.
+                  </h6>
+                  <a href="https://www.youtube.com/embed/diIOyfWTaB0" class="btn btn-primary">Video</a>
+              </div>
+          </div>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="card" style="width: 18rem;">
+            <div class="ratio ratio-4x3">
+                <iframe width="288" height="200" src="https://www.youtube.com/embed/diIOyfWTaB0"
+                    title="Teaser video" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+        gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Como crear en post en la landing page?</h5>
+                <br>
+                <br>
+                <h6 class="card-text">Si no sabes como crear un post en la landing page en el video te explicamos como puedes hacerlo.
+                </h6>
+                <a href="https://www.youtube.com/embed/diIOyfWTaB0" class="btn btn-primary">Video</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-3">
+      <div class="card" style="width: 18rem;">
+          <div class="ratio ratio-4x3">
+              <iframe width="288" height="200" src="https://www.youtube.com/embed/diIOyfWTaB0"
+                  title="Teaser video" frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media;
+      gyroscope; picture-in-picture"
+                  allowfullscreen></iframe>
+          </div>
+          <div class="card-body">
+              <h5 class="card-title">Como crear en post en la landing page?</h5>
+              <br>
+              <br>
+              <h6 class="card-text">Si no sabes como crear un post en la landing page en el video te explicamos como puedes hacerlo.
+              </h6>
+              <a href="https://www.youtube.com/embed/diIOyfWTaB0" class="btn btn-primary">Video</a>
+          </div>
+      </div>
+  </div>
+    </div>
+
+
+
+    @stop
