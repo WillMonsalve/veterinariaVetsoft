2d07824fca87f434941281515986c10f9c25bc05
JuanTrP <juan.fe19@hotmail.com>
2022-12-09 04:36:24 -0500

    Problemas resultos

--- resources/views/admin/citas/index.blade.php
+++ resources/views/admin/citas/index.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Citas')
 
 @section('css')
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'usuarios')
+@section('title', 'Usuarios')
 
 @section('css')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
